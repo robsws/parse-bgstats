2c098c1c9331cd43dbb4374056f823e305a7576c
Rob Streeting <robsws@gmail.com>
2019-01-19 21:00:06 +0000

    Can now parse the json in the file to a dict.

--- parse-bgstats.py
+++ parse-bgstats.py
@@ -3,8 +3,11 @@ import sys
 import argparse
 
 args_def = argparse.ArgumentParser(description='Parse backup files from BGStats app.')
-args_def.add_argument('filename', type=argparse.FileType('r'), help='File from which to pull the JSON data.')
+args_def.add_argument('file', type=argparse.FileType('r'), help='File from which to pull the JSON data.')
 args = args_def.parse_args()
 
-print(args)
+json_data = args.file.readlines()[0]
+bgstats = json.loads(json_data)
 
+outfile = open('out.txt', 'w')
+outfile.write(json.dumps(bgstats, indent=2))
